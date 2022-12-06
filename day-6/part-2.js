df0f9ac00727b87c5dffd526122a33e82f06faf5
James <jamesglendenning1@gmail.com>
2022-12-06 23:33:13 +0000

    Update part-2.js

--- day-6/part-2.js
+++ day-6/part-2.js
@@ -10,13 +10,16 @@ class index {
 	 */
 	constructor( fileName = 'test.txt' ) {
 
+		this.answer = 0;
+
 		this.initialise( fileName );
 	}
 
 	async initialise( fileName ) {
 
 		// Read file.
-		this.dataArray = await asyncReadFile( fileName );
+		this.dataArray = await asyncReadFile( fileName )
+		this.dataArray = this.dataArray[0].split('');
 
 		// Loop logic.
 		this.loopItems();
@@ -27,11 +30,44 @@ class index {
 
 	loopItems() {
 		
+		let atIndex = 0;
+
 		// this.dataArray
+		for (let index = 0; index < this.dataArray.length; index++) {
+			const letter = this.dataArray[index];
+			
+			// 4, 8, 12, 16
+			// r, r, w, c
+
+			if ( index > 3 ) {
+
+				const lastFourLetters = [
+					this.dataArray[index -1],
+					this.dataArray[index -2],
+					this.dataArray[index -3],
+					this.dataArray[index],
+				];
+
+				// First find the duplicates.
+				const hasDuplicates = lastFourLetters.filter((item, i, ar) => ar.indexOf(item) === i);
+
+				if ( 4 === hasDuplicates.length ) {
+
+					atIndex = index + 1;
+					break;
+				}
+			}
+		}
+
+		this.answer = atIndex;
+	}
+
+	uniq(a) {
+		return Array.from(new Set(a));
 	}
 
 	outputMyScore() {
-		console.log( `` );
+		console.log( `The answer is: ${this.answer}` );
 	}
 }
 
