65ad270d6a1cb5cda5a0a6b11da8b7d2e7cb08dd
James <jamesglendenning1@gmail.com>
2022-12-06 23:35:59 +0000

    day 6 part 2 complete

--- day-6/part-2.js
+++ day-6/part-2.js
@@ -39,19 +39,29 @@ class index {
 			// 4, 8, 12, 16
 			// r, r, w, c
 
-			if ( index > 3 ) {
+			if ( index > 14 ) {
 
 				const lastFourLetters = [
-					this.dataArray[index -1],
-					this.dataArray[index -2],
-					this.dataArray[index -3],
+					this.dataArray[index - 13],
+					this.dataArray[index - 12],
+					this.dataArray[index - 11],
+					this.dataArray[index - 10],
+					this.dataArray[index - 9],
+					this.dataArray[index - 8],
+					this.dataArray[index -7],
+					this.dataArray[index - 6],
+					this.dataArray[index - 5],
+					this.dataArray[index - 4],
+					this.dataArray[index - 3],
+					this.dataArray[index - 2],
+					this.dataArray[index - 1],
 					this.dataArray[index],
 				];
 
 				// First find the duplicates.
 				const hasDuplicates = lastFourLetters.filter((item, i, ar) => ar.indexOf(item) === i);
 
-				if ( 4 === hasDuplicates.length ) {
+				if ( 14 === hasDuplicates.length ) {
 
 					atIndex = index + 1;
 					break;
